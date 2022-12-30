f4caf40597d8f73c7e5b1281d65b4ab40ae012a8
Павел Полищук <stiksxdi@yandex.ru>
2022-12-30 21:25:16 +0300

    Финальные правки

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -13,7 +13,10 @@ from ..models import Comment, Group, Post
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -27,6 +30,11 @@ class PostFormTests(TestCase):
             description='Тестовое описание группы',
         )
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def setUp(self):
         self.guest_client = Client()
         self.authorized_user = Client()
@@ -35,9 +43,23 @@ class PostFormTests(TestCase):
     def test_authorized_user_create_post(self):
         """Проверка создания записи авторизированным клиентом."""
         posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {
             'text': 'Текст поста',
             'group': self.group.id,
+            'image': uploaded,
         }
         response = self.authorized_user.post(
             reverse('posts:post_create'),
@@ -55,6 +77,7 @@ class PostFormTests(TestCase):
         self.assertEqual(post.text, form_data['text'])
         self.assertEqual(post.author, self.post_author)
         self.assertEqual(post.group_id, form_data['group'])
+        self.assertEqual(post.image.name, 'posts/small.gif')
 
     def test_authorized_user_edit_post(self):
         """Проверка редактирования записи авторизированным клиентом."""
